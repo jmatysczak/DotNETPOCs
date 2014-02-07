f0950f8301de36d24a5fcead7983d03c64908c23
jmat <jmat@alum.mit.edu>
2014-02-06 20:49:15 -0500

    Added example of proxying an interface.

---
+++ InterfaceProxying/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Proxies;
+
+namespace InterfaceProxying {
+  class Program {
+    static void Main(string[] args) {
+      (new ExampleProxy(typeof(InterfaceToProxy)).GetTransparentProxy() as InterfaceToProxy).AnEvent += (sender, e) => { };
+    }
+  }
+
+  interface InterfaceToProxy {
+    event EventHandler AnEvent;
+  }
+
+  class ExampleProxy : RealProxy {
+    public ExampleProxy(Type t) : base(t) { }
+
+    public override IMessage Invoke(IMessage msg) {
+      Console.WriteLine("Properties:");
+      foreach(object key in msg.Properties.Keys) {
+        Console.WriteLine("   {0}: {1}", key, msg.Properties[key]);
+      }
+
+      IMethodCallMessage call = (IMethodCallMessage)msg;
+      Console.WriteLine("Method Call Properties:");
+      Console.WriteLine("   ArgCount: {0}", call.ArgCount);
+      Console.WriteLine("   Args: {0}", call.Args);
+      Console.WriteLine("   HasVarArgs: {0}", call.HasVarArgs);
+      Console.WriteLine("   InArgCount: {0}", call.InArgCount);
+      Console.WriteLine("   InArgs: {0}", call.InArgs);
+      Console.WriteLine("   LogicalCallContext: {0}", call.LogicalCallContext);
+      Console.WriteLine("   MethodBase: {0}", call.MethodBase);
+      Console.WriteLine("   MethodName: {0}", call.MethodName);
+      Console.WriteLine("   MethodSignature: {0}", call.MethodSignature);
+      Console.WriteLine("   TypeName: {0}", call.TypeName);
+      Console.WriteLine("   Uri: {0}", call.Uri);
+
+      return new ReturnMessage(null, null, 0, call.LogicalCallContext, call);
+    }
+  }
+}
--- InterfaceProxying/Properties/AssemblyInfo.cs
+++ InterfaceProxying/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ASPNETPOCs")]
+[assembly: AssemblyTitle("InterfaceProxying")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ASPNETPOCs")]
+[assembly: AssemblyProduct("InterfaceProxying")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
+[assembly: Guid("204099ae-75d9-4989-9e9a-8a9e482bee72")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,7 +28,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
