bf95a63a92649fd372c338f89f80a505332c010b
jmat <jmat@alum.mit.edu>
2014-02-17 21:50:41 -0500

    Added comments explaining the code.

--- ASPNETHosting/Program.cs
+++ ASPNETHosting/Program.cs
@@ -13,20 +13,33 @@ namespace ASPNETHosting {
 
       try {
         var currentDir = Directory.GetCurrentDirectory();
+
+        // The web app root and its bin directory of the app this program will host.
+        // This will be under the directory this exe is running in.
         var aspnetRootDir = Path.Combine(currentDir, "aspnet");
         var aspnetRootBinDir = Path.Combine(aspnetRootDir, "bin");
 
+        // Delete the web app root directory if it exists and create it.
         if(Directory.Exists(aspnetRootDir)) Directory.Delete(aspnetRootDir, true);
         Directory.CreateDirectory(aspnetRootBinDir);
 
+        // The directory of the web app we're going to use. We're going to copy the necessary files from it to aspnetRootDir and aspnetRootBinDir.
+        // You can just use the directory where the web app exists, but you have to be careful about cleaning up any modifications to its web.config.
         var aspnetSourceDir = Path.Combine(currentDir.Substring(0, currentDir.IndexOf("ASPNETHosting")), "ASPNETPOCs");
+        // Copy the web app artifacts we want.
         File.Copy(Path.Combine(Path.Combine(aspnetSourceDir, "bin"), "ASPNETPOCs.dll"), Path.Combine(aspnetRootBinDir, "ASPNETPOCs.dll"));
         File.Copy(Path.Combine(Path.Combine(aspnetSourceDir, "DISetupOnPageParse"), "DIPage.aspx"), Path.Combine(aspnetRootDir, "DIPage.aspx"));
         File.Copy(Path.Combine(Path.Combine(aspnetSourceDir, "DISetupOnPageParse"), "DIUserControl.ascx"), Path.Combine(aspnetRootDir, "DIUserControl.ascx"));
 
+        // Put a copy of this exe in the web app's bin directory.
+        // This is just a short cut.
+        // You can use a separate dll to hold the host (ExampleAspNetHost) and any other code (ExampleHttpModule).
         var thisAssemblysLocation = Assembly.GetExecutingAssembly().Location;
         File.Copy(thisAssemblysLocation, Path.Combine(aspnetRootBinDir, Path.GetFileName(thisAssemblysLocation)));
 
+        // Customize the web.config.
+        // In this example we're writing the entire web.config because the target web app doesn't have one.
+        // If you web app has a web.config, open it, modify it, and overwrite it.
         File.WriteAllText(Path.Combine(aspnetRootDir, "web.config"), @"
 <configuration>
   <system.web>
@@ -37,8 +50,13 @@ namespace ASPNETHosting {
 </configuration>
         ");
 
+        // Create the asp.net host.
+        // "ExampleAspNetHost" must be in an assembly in the bin (or the gac, but I haven't tried this).
+        // "ExampleVirtualRoot" is arbirary.
+        // "aspnetRootDir" is the web app root.
         host = (ExampleAspNetHost)ApplicationHost.CreateApplicationHost(typeof(ExampleAspNetHost), "/ExampleVirtualRoot", aspnetRootDir);
 
+        // Process a page.
         host.ProcessRequest("DIPage.aspx");
       } finally {
         if(host != null) {
