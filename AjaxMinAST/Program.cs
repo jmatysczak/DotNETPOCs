41ed18c4accfcca4fb180b23c69ddee298a226df
jmat <jmat@alum.mit.edu>
2014-03-04 22:40:55 -0500

    Parse an object literal to hold the input functions.
    Use a tree visitor to find the parsed literal.
    Insert the functions into the literal.
    Need to set the names correctly.

--- AjaxMinAST/Program.cs
+++ AjaxMinAST/Program.cs
@@ -18,12 +18,31 @@ namespace AjaxMinAST {
 			Console.WriteLine("--- AST ---");
 			var parser = new JSParser();
 			parser.CompilerError += (_, ea) => Console.WriteLine(ea.Error);
-			var block = parser.Parse(input);
-			new Visitor().Visit(block);
+			
+			var functions = parser.Parse(input);
+			var functionContext = parser.Parse("var functionContext = {};");
+
+			new ObjectLiteralVisitor(functions).Visit(functionContext);
+
+			OutputVisitor.Apply(Console.Out, functionContext, new CodeSettings() { MinifyCode = false, OutputMode = OutputMode.MultipleLines });
 		}
 	}
 
-	class Visitor : TreeVisitor {
+	class ObjectLiteralVisitor : TreeVisitor {
+		Block functions;
 
+		public ObjectLiteralVisitor(Block functions) {
+			this.functions = functions;
+		}
+	
+		public override void Visit(ObjectLiteral node) {
+			base.Visit(node);
+			foreach(var function in this.functions.Children) {
+				node.Properties.Insert(node.Properties.Count, new ObjectLiteralProperty(node.Context) {
+					Name = new ObjectLiteralField("Name", PrimitiveType.String, node.Context),
+					Value = function
+				});
+			}
+		}
 	}
 }
